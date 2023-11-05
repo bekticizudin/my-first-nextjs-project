c80fb8b346c0deec55ebaffb2892e6d085112d37
bekticizudin <izudinbektic30@gmail.com>
2023-11-05 22:51:56 +0100

    bug fix on paginationButtons

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,21 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from './UserContext';
 
 const Pagination: React.FC = () => {
-  const { currentPage, totalUsers, setCurrentPage } = useContext(UserContext);
+  const { currentPage, totalUsers, setCurrentPage, users } = useContext(UserContext);
   const totalPages = Math.ceil(totalUsers.length / 5);
+
+  const [searchResults, setSearchResults] = useState(false);
+
+  useEffect(() => {
+    setSearchResults(users.length > 0);
+  } );
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
+  const displayPagination = totalPages > 1 && users.length > 4;
+
   return (
     <div>
-      {Array.from({ length: totalPages }, (_, i) => (
-        <button key={i} onClick={() => handlePageChange(i + 1)} disabled={currentPage === i + 1}>
-          {i + 1}
-        </button>
-      ))}
+      {displayPagination && (
+        <>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button key={i} onClick={() => handlePageChange(i + 1)} disabled={currentPage === i + 1}>
+              {i + 1}
+            </button>
+          ))}
+        </>
+      )}
     </div>
   );
 };
